a049bb188c3bf313f426842b45e3d18b1023b99a
Laxmikant <lalubhutad@gmail.com>
2022-08-18 18:04:17 +0530

    add new changes in the code

--- Addition.java
+++ Addition.java
@@ -3,9 +3,9 @@ public class Addition
     public static void main(String args[])
     {
 
-        int a =10;
-        int b = 20;
-        int c = a+b;
+        int a =40;
+        int b = 30;
+        int c = a-b;
         System.out.println(c);
     }
 }
